9267bf1b9af54167c0057609ab2fa75ae8f7ee25
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-02-27 11:50:27 +0000

    [finish]2-24

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
     redirect_to post_image_path(post_image)
   end
 
-  def destory
+  def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
